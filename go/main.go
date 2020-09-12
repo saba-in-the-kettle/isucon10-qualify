dc38dee5da21c566711d1ec08732225740cd21c7
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-09-12 18:17:32 +0900

    Revert "recoキャッシュ"

--- go/main.go
+++ go/main.go
@@ -13,7 +13,6 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/goccy/go-json"
 
@@ -683,11 +682,6 @@ func postEstate(c echo.Context) error {
 		c.Logger().Errorf("failed to commit tx: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
-
-	recoMap.mu.Lock()
-	defer recoMap.mu.Unlock()
-	recoMap.rm = map[int][]Estate{}
-
 	return c.NoContent(http.StatusCreated)
 }
 
@@ -797,26 +791,11 @@ func getLowPricedEstate(c echo.Context) error {
 	return c.JSONBlob(http.StatusOK, b)
 }
 
-var recoMap = struct {
-	rm map[int][]Estate
-	mu sync.RWMutex
-}{
-	rm: map[int][]Estate{},
-	mu: sync.RWMutex{},
-}
-
 func searchRecommendedEstateWithChair(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.Logger().Infof("Invalid format searchRecommendedEstateWithChair id : %v", err)
 		return c.NoContent(http.StatusBadRequest)
-	}
-
-	recoMap.mu.RLock()
-
-	if value, ok := recoMap.rm[id]; ok {
-		recoMap.mu.RUnlock()
-		return c.JSON(http.StatusOK, EstateListResponse{Estates: value})
 	}
 
 	chair := Chair{}
@@ -825,11 +804,9 @@ func searchRecommendedEstateWithChair(c echo.Context) error {
 	if err != nil {
 		if err == sql.ErrNoRows {
 			c.Logger().Infof("Requested chair id \"%v\" not found", id)
-			recoMap.mu.RUnlock()
 			return c.NoContent(http.StatusBadRequest)
 		}
 		c.Logger().Errorf("Database execution error : %v", err)
-		recoMap.mu.RUnlock()
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
@@ -841,18 +818,12 @@ func searchRecommendedEstateWithChair(c echo.Context) error {
 	err = db.Select(&estates, query, w, h, w, d, h, w, h, d, d, w, d, h, Limit)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			recoMap.mu.RUnlock()
 			return c.JSON(http.StatusOK, EstateListResponse{[]Estate{}})
 		}
 		c.Logger().Errorf("Database execution error : %v", err)
-		recoMap.mu.RUnlock()
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	recoMap.mu.Lock()
-	defer recoMap.mu.Unlock()
-
-	recoMap.rm[id] = estates
 	return c.JSON(http.StatusOK, EstateListResponse{Estates: estates})
 }
 
